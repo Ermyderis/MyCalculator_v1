c922bec15de6f83024b8e2239669c371c8cff696
dovyd <dovydas0000@gmail.com>
2020-10-30 20:44:46 +0200

    Described data in MainActivity

--- app/src/main/java/com/example/mycalculator_v1/MainActivity.java
+++ app/src/main/java/com/example/mycalculator_v1/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.mycalculator_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonDot, button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonCE, buttonDEL, buttonDivision, buttonMultiplication, buttonroot, buttonMinus, buttonPlus, buttonEqual;
+    TextView screen;
+    String process;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        buttonDivision = findViewById(R.id.buttonDivision);
+        buttonMultiplication = findViewById(R.id.buttonMultiplication);
+        buttonroot = findViewById(R.id.buttonroot);
+
+        buttonEqual = findViewById(R.id.buttonEqual);
+        buttonDot = findViewById(R.id.buttonDot);
+
+        buttonCE = findViewById(R.id.buttonCE); //del all
+        buttonDEL = findViewById(R.id.buttonDEL); //del one character
+
+        screen = findViewById(R.id.screen);
+
     }
+
+
 }
